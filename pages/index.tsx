de4b47bba21c2fd2c047e0cc44c8fd93d9a382b0
John Chen <johnhuichen@gmail.com>
2020-12-14 16:25:07 -0500

    Implement searchbar in home

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,8 @@
+import { useMemo } from "react";
 import Head from "next/head";
 import Link from "next/link";
 import { GetStaticProps } from "next";
+import debounce from "lodash/debounce";
 
 import { getGames, HomePageGame } from "lib/home";
 
@@ -35,6 +37,26 @@ const GameCard: React.FC<HomePageGame> = ({
 };
 
 const Home: React.FC<Props> = ({ games }: Props) => {
+  const handleChangeInput = useMemo(
+    () =>
+      // debounce(async (e: { target: HTMLInputElement }) => {
+      debounce(() => {
+        // const { value: query } = e.target;
+        setSearchResult([]);
+
+        // const response = (await fetch("/api/hello", {
+        //   method: "POST",
+        //   headers: {
+        //     "Content-Type": "application/json",
+        //   },
+        //   body: JSON.stringify({ query }),
+        // }).then(res => res.json())) as SearchResponse;
+
+        // setSearchResult(response.result);
+      }, 300),
+    []
+  );
+
   return (
     <>
       <Head>
@@ -42,16 +64,26 @@ const Home: React.FC<Props> = ({ games }: Props) => {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div className={styles.container}>
-        {games.map(game => (
-          <GameCard
-            key={`game-${game.id}`}
-            id={game.id}
-            name={game.name}
-            imgFile={game.imgFile}
-            shortDescription={game.shortDescription}
-            gameType={game.gameType}
+        <div className={styles.searchContainer}>
+          <i aria-hidden className="fas fa-search" />
+          <input
+            className={styles.searchInput}
+            placeholder="You can find it by searching too"
+            onChange={handleChangeInput}
           />
-        ))}
+        </div>
+        <div className={styles.gameContianer}>
+          {games.map(game => (
+            <GameCard
+              key={`game-${game.id}`}
+              id={game.id}
+              name={game.name}
+              imgFile={game.imgFile}
+              shortDescription={game.shortDescription}
+              gameType={game.gameType}
+            />
+          ))}
+        </div>
       </div>
     </>
   );
