fc22021caee1518ccc90e4c554525d6d4ab6c1cd
John Chen <johnhuichen@gmail.com>
2020-12-25 08:57:38 -0500

    Update css

--- components/shared/GameCard/GameCard.tsx
+++ components/shared/GameCard/GameCard.tsx
@@ -34,7 +34,15 @@ const GameCard: React.FC<Props> = ({
   return (
     <div className={styles.container}>
       <div className={styles.gameCard}>
-        <img className={styles.img} src={src} alt={`${name}-avatar`} />
+        <Link href={`/${gameType}/${id}`}>
+          <img
+            className={styles.img}
+            src={src}
+            alt={`${name}-avatar`}
+            width="250px"
+            height="250px"
+          />
+        </Link>
         <div className={styles.textWrapper}>
           <Link href={`/${gameType}/${id}`}>
             <a href={`/${gameType}/${id}`} className={styles.title}>
